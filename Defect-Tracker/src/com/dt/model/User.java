71bd15c079105649d6d9eb92e1d97d4e046852c6
Abhishek Rattihalli <rhabhishek_setty@ymail.com>
2016-10-28 18:23:33 -0400

    Pushing User Class

---
+++ Defect-Tracker/src/com/dt/model/User.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.dt.model;
+
+/**
+ * @author Abhishek
+ *
+ */
+public class User {
+
+	private String userName;
+	private String email;
+	private String role;
+	private String module;
+	private String password;
+	
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	/**
+	 * @return the module
+	 */
+	public String getModule() {
+		return module;
+	}
+	/**
+	 * @param module the module to set
+	 */
+	public void setModule(String module) {
+		this.module = module;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
