d7f337d3357ec25a35ec99406e0ca637a073d852
Anurag Tayade <atayade1@uncc.edu>
2016-11-01 13:02:31 -0400

    test case

---
+++ Defect-Tracker/src/com/dt/addUser/AddUserControllerTest.java
@@ -0,0 +1,14 @@
+package com.dt.addUser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddUserControllerTest {
+
+	@Test
+	public void testAddUserController() {
+		fail("Not yet implemented");
+	}
+
+}
