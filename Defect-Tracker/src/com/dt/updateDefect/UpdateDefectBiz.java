fd7437e85f0a6a40e60724efc2e8af1b9542f660
Anurag Tayade <atayade1@uncc.edu>
2016-11-11 22:40:42 -0500

    update defect changes

--- Defect-Tracker/src/com/dt/updateDefect/UpdateDefectBiz.java
+++ Defect-Tracker/src/com/dt/updateDefect/UpdateDefectBiz.java
@@ -1,5 +1,29 @@
 package com.dt.updateDefect;
 
-public class UpdateDefectBiz {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.dt.addDefect.AddDefectDao;
+import com.dt.model.Defect;
+
+public class UpdateDefectBiz {
+	public int updateDefect(HttpServletRequest request){
+		 Defect obj=new Defect();
+		 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:MM");
+		 Date date = new Date();
+		 obj.setAssigned_date(dateFormat.format(date));
+		 obj.setDefect_name(request.getParameter("defect_name"));
+		 obj.setDefect_desc(request.getParameter("defect_desc"));
+		 obj.setAssigned_to(request.getParameter("assigned_to"));
+		 obj.setDefect_status(request.getParameter("defect_status"));
+		 obj.setDefect_type(request.getParameter("defect_type"));
+		 obj.setSeverity(request.getParameter("severity"));
+		 obj.setModule_id(Integer.parseInt(request.getParameter("module_id")));
+		 AddDefectDao dao=new AddDefectDao();
+		 int flag=dao.addDefect(obj);
+		 return flag;
+	 }
 }
--- Defect-Tracker/src/com/dt/updateDefect/UpdateDefectController.java
+++ Defect-Tracker/src/com/dt/updateDefect/UpdateDefectController.java
@@ -1,5 +1,38 @@
 package com.dt.updateDefect;
 
-public class UpdateDefectController {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class UpdateDefectController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public UpdateDefectController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		UpdateDefectBiz obj=new UpdateDefectBiz();
+		int flag=obj.updateDefect(request);
+		if(flag==1)
+		{
+			out.println("Defect updated successfully");
+		}
+		doGet(request, response);
+	}
 
 }
